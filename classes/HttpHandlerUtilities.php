8581920ac0013d144bcb7c7d8a1c930183cbbe06
Léo Paillard <leo.paillard@gmail.com>
2023-01-18 00:14:18 +0100

    add fetch route path param function

--- classes/HttpHandlerUtilities.php
+++ classes/HttpHandlerUtilities.php
@@ -8,4 +8,38 @@ abstract class HttpHandlerUtilities
         header('Content-Type: application/json');
         echo json_encode(['Success' => $state]);
     }
+
+    public static function fetchRouteAndPathParameters(string $route): array {
+
+        $routeExplode = explode('/', $route);
+
+        if (!in_array($routeExplode[1] ,["reports", "devices", "locations", "users"])) {
+            // Route invalide
+            header("HTTP/1.0 404 Not Found");
+            self::setHTTPResponse(404, false);
+            exit();
+        }
+
+        if (count($routeExplode) == 4) {
+            $routeArray['route_base'] = $routeExplode[1];
+            $routeArray['location'] = $routeExplode[2];
+            $routeArray['range'] = $routeExplode[3];
+        } elseif (count($routeExplode) == 3) {
+            $routeArray['route_base'] = $routeExplode[1];
+            if (is_numeric($routeExplode[2])) {
+                $routeArray['id'] = $routeExplode[2];
+            } else {
+                $routeArray['location'] = $routeExplode[2];
+            }
+        } elseif (count($routeExplode) == 2) {
+            $routeArray['route_base'] = $routeExplode[1];
+        } else {
+            // Route invalide
+            header("HTTP/1.0 404 Not Found");
+            self::setHTTPResponse(404, false);
+            exit();
+        }
+
+        return $routeArray;
+    }
 }
