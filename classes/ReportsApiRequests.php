0e3d60e9ff044b2226390f8d9a9a949319c9f43f
Léo Paillard <leo.paillard@gmail.com>
2023-01-18 00:27:18 +0100

    clean file

--- classes/ReportsApiRequests.php
+++ classes/ReportsApiRequests.php
@@ -16,32 +16,25 @@ class ReportsApiRequests
         $reportValues = json_decode(file_get_contents("php://input"), true);
 
         if (isset($reportValues['temperature']) && isset($reportValues['humidity']) && isset($reportValues['deviceUuid']) && isset($reportValues['locationName'])) {
-            // Récupération des données envoyées par l'esp8266
             $temperature = $reportValues['temperature'];
             $humidity = $reportValues['humidity'];
             $deviceUuid = $reportValues['deviceUuid'];
             $locationName = $reportValues['locationName'];
 
-            // Préparation de la requête d'insertion
             $query = $this->pdo->prepare('INSERT INTO REPORT (temperature, humidity, dateTime, deviceUuid, locationName) VALUES (:temperature, :humidity, NOW(), :deviceUuid, :locationName)');
 
-            // Liaison des paramètres
             $query->bindParam(':temperature', $temperature);
             $query->bindParam(':humidity', $humidity);
             $query->bindParam(':deviceUuid', $deviceUuid);
             $query->bindParam(':locationName', $locationName);
 
-            // Exécution de la requête
             try {
                 $query->execute();
-                // Si la requête s'est bien exécutée, on renvoie un code de succès (200)
                 HttpHandlerUtilities::setHTTPResponse(200, True);
             } catch (PDOException $e) {
-                // Si la requête échoue, on renvoie un code d'erreur (500)
                 HttpHandlerUtilities::setHTTPResponse(500, False);
             }
         } else {
-            // Si les données sont incomplètes, on renvoie un code d'erreur (400)
             HttpHandlerUtilities::setHTTPResponse(401, False);
         }
 
@@ -49,20 +42,14 @@ class ReportsApiRequests
 
     public function getReport(string $id): void
     {
-        // Préparation de la requête de sélection
         $query = $this->pdo->prepare('SELECT * FROM REPORT WHERE (reportId = :reportId)');
 
-        // Exécution de la requête
         try {
             $query->execute(['reportId' => $id]);
-            // Récupération des résultats de la requête sous forme de tableau associatif
             $results = $query->fetchAll(PDO::FETCH_ASSOC);
-            // Si la requête s'est bien exécutée, on renvoie un code de succès (200)
             http_response_code(200);
-            // Encodage des résultats en JSON et renvoi de la réponse
             echo json_encode($results);
         } catch (PDOException $e) {
-            // Si la requête échoue, on renvoie un code d'erreur (500)
             HttpHandlerUtilities::setHTTPResponse(500, False);
         }
     }
@@ -70,20 +57,14 @@ class ReportsApiRequests
     public function getLastsReports(): void
     {
 
-        // Préparation de la requête de sélection
         $query = $this->pdo->prepare('SELECT * FROM REPORT ORDER BY reportId DESC LIMIT 10;');
 
-        // Exécution de la requête
         try {
             $query->execute();
-            // Récupération des résultats de la requête sous forme de tableau associatif
             $results = $query->fetchAll(PDO::FETCH_ASSOC);
-            // Si la requête s'est bien exécutée, on renvoie un code de succès (200)
             http_response_code(200);
-            // Encodage des résultats en JSON et renvoi de la réponse
             echo json_encode($results);
         } catch (PDOException $e) {
-            // Si la requête échoue, on renvoie un code d'erreur (500)
             HttpHandlerUtilities::setHTTPResponse(500, False);
         }
     }
@@ -140,23 +121,19 @@ class ReportsApiRequests
             $temperature = $_PUT['temperature'];
             $humidity = $_PUT['humidity'];
 
-            // Mise à jour du relevé de température et d'humidité dans la base de données
             $query = $this->pdo->prepare('UPDATE REPORT SET temperature = :temperature, humidity = :humidity WHERE reportId = :reportId');
 
             try {
 
                 $query->execute(['temperature' => $temperature, 'humidity' => $humidity, 'reportId' => $id]);
 
-                // Si la requête s'est bien exécutée, on renvoie un code de succès (200)
                 HttpHandlerUtilities::setHTTPResponse(200, True);
 
             } catch (PDOException $e) {
-                // Si la requête échoue, on renvoie un code d'erreur (500)
                 HttpHandlerUtilities::setHTTPResponse(500, False);
             }
 
         } else {
-            // Si les données sont incomplètes, on renvoie un code d'erreur (400)
             HttpHandlerUtilities::setHTTPResponse(400, False);
         }
 
@@ -165,17 +142,14 @@ class ReportsApiRequests
     public function deleteReport(string $id): void
     {
 
-        // Suppression du relevé de température et d'humidité de la base de données
         $query = $this->pdo->prepare('DELETE FROM REPORT WHERE reportId = :reportId');
 
         try {
 
             $query->execute(['reportId' => $id]);
-            // Si la requête s'est bien exécutée, on renvoie un code de succès (200)
             HttpHandlerUtilities::setHTTPResponse(200, True);
 
         } catch (PDOException $e) {
-            // Si la requête échoue, on renvoie un code d'erreur (500)
             HttpHandlerUtilities::setHTTPResponse(500, False);
         }
     }
