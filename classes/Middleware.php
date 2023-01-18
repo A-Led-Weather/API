cfc0255cd97ee66367add723e5b41c6b6ef347d5
Léo Paillard <leo.paillard@gmail.com>
2023-01-18 12:56:25 +0100

    add createToken method

--- classes/Middleware.php
+++ classes/Middleware.php
@@ -2,37 +2,43 @@
 
 abstract class Middleware
 {
+    public function createToken(): string
+    {
+        $tokenBytes = random_bytes(16);
+        return bin2hex($tokenBytes);
+    }
+
     public static function fetchRoutePathParameters(string $route): array
     {
 
         $routeExplode = explode('/', $route);
 
         if (!in_array($routeExplode[1], ["reports", "devices", "locations", "users"])) {
-            // Route invalide
-            header("HTTP/1.0 404 Not Found");
             self::setHTTPResponse(404, "Route not found", "HTTP/1.0 404 Not Found", true);
             exit();
         }
 
-        if (count($routeExplode) == 4) {
-            $routeArray['route_base'] = $routeExplode[1];
-            $routeArray['location'] = $routeExplode[2];
-            $routeArray['range'] = $routeExplode[3];
-        } elseif (count($routeExplode) == 3) {
-            $routeArray['route_base'] = $routeExplode[1];
-            if (is_numeric($routeExplode[2])) {
-                $routeArray['id'] = $routeExplode[2];
-            } else {
+        switch (count($routeExplode)) {
+            case 4:
+                $routeArray['route_base'] = $routeExplode[1];
                 $routeArray['location'] = $routeExplode[2];
-            }
-        } elseif (count($routeExplode) == 2) {
-            $routeArray['route_base'] = $routeExplode[1];
-        } else {
-            // Route invalide
-            self::setHTTPResponse(404, "Route not found", "HTTP/1.0 404 Not Found", true);
-            exit();
+                $routeArray['range'] = $routeExplode[3];
+                break;
+            case 3:
+                $routeArray['route_base'] = $routeExplode[1];
+                if (is_numeric($routeExplode[2])) {
+                    $routeArray['id'] = $routeExplode[2];
+                } else {
+                    $routeArray['location'] = $routeExplode[2];
+                }
+                break;
+            case 2:
+                $routeArray['route_base'] = $routeExplode[1];
+                break;
+            default:
+                self::setHTTPResponse(404, "Route not found", "HTTP/1.0 404 Not Found", true);
+                exit();
         }
-
         return $routeArray;
     }
 
