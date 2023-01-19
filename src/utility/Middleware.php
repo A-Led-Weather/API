8dade26f542a42797a844c99d53232484b63250e
Léo Paillard <leo.paillard@gmail.com>
2023-01-20 00:26:59 +0100

    Start Implement JWT and delete fetchRoutePathParameters (using fastroute)

--- src/utility/Middleware.php
+++ src/utility/Middleware.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace utility;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 abstract class Middleware
 {
     public static function createToken(): string
@@ -23,46 +25,6 @@ abstract class Middleware
         }
     }
 
-    public static function fetchRoutePathParameters(string $route): array
-    {
-
-        $routeExplode = explode('/', $route);
-
-        if (!in_array($routeExplode[1], ["reports", "devices", "locations", "users"])) {
-            self::setHTTPResponse(404, "Route not found", true);
-            exit();
-        }
-
-        switch (count($routeExplode)) {
-            case 4:
-                $routeArray['route_base'] = $routeExplode[1];
-                $routeArray['location'] = $routeExplode[2];
-                $routeArray['range'] = $routeExplode[3];
-                break;
-            case 3:
-                $routeArray['route_base'] = $routeExplode[1];
-                if (is_numeric($routeExplode[2])) {
-                    $routeArray['id'] = $routeExplode[2];
-                } else {
-                    if (!strpos($routeExplode[2], '@') && $routeExplode[2] != 'login') {
-                        $routeArray['location'] = $routeExplode[2];
-                    } elseif ($routeExplode[2] == 'login')
-                        $routeArray['login'] = true;
-                    {
-                        $routeArray['email'] = $routeExplode[2];
-                    }
-                }
-                break;
-            case 2:
-                $routeArray['route_base'] = $routeExplode[1];
-                break;
-            default:
-                self::setHTTPResponse(404, "Route not found", true);
-                exit();
-        }
-        return $routeArray;
-    }
-
     public static function setHTTPResponse(int $httpCode, mixed $state, bool $sendRequestState): void
     {
         http_response_code($httpCode);
@@ -71,4 +33,5 @@ abstract class Middleware
             echo json_encode(['result' => $state]);
         }
     }
+
 }
