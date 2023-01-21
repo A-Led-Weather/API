b0c5789b7feb28365330f5a95e127fa82f02e669
Léo Paillard <leo.paillard@gmail.com>
2023-01-21 01:17:55 +0100

    Reformat code

--- src/router/Router.php
+++ src/router/Router.php
@@ -26,17 +26,39 @@ class Router
     {
 
         $dispatcher = simpleDispatcher(function (RouteCollector $r) {
-            $r->addRoute(self::GET_LAST_REPORTS['method'], self::GET_LAST_REPORTS['uri'], self::GET_LAST_REPORTS['request']);
-            $r->addRoute(self::ADD_REPORT['method'], self::ADD_REPORT['uri'], self::ADD_REPORT['request']);
-            $r->addRoute(self::GET_REPORT_BY_ID['method'], self::GET_REPORT_BY_ID['uri'], self::GET_REPORT_BY_ID['request']);
-            $r->addRoute(self::DELETE_REPORT['method'], self::DELETE_REPORT['uri'], self::DELETE_REPORT['request']);
-            $r->addRoute(self::GET_LAST_REPORT_BY_LOCATION['method'], self::GET_LAST_REPORT_BY_LOCATION['uri'], self::GET_LAST_REPORT_BY_LOCATION['request']);
-            $r->addRoute(self::GET_REPORTS_BY_LOCATION_BY_TIME_RANGE['method'], self::GET_REPORTS_BY_LOCATION_BY_TIME_RANGE['uri'], self::GET_REPORTS_BY_LOCATION_BY_TIME_RANGE['request']);
-            $r->addRoute(self::ADD_USER['method'], self::ADD_USER['uri'], self::ADD_USER['request']);
-            $r->addRoute(self::GET_USER_BY_EMAIL['method'], self::GET_USER_BY_EMAIL['uri'], self::GET_USER_BY_EMAIL['request']);
-            $r->addRoute(self::UPDATE_USER['method'], self::UPDATE_USER['uri'], self::UPDATE_USER['request']);
-            $r->addRoute(self::DELETE_USER['method'], self::DELETE_USER['uri'], self::DELETE_USER['request']);
-            $r->addRoute(self::AUTHENTICATE_USER['method'], self::AUTHENTICATE_USER['uri'], self::AUTHENTICATE_USER['request']);
+            $r->addRoute(self::GET_LAST_REPORTS['method'],
+                self::GET_LAST_REPORTS['uri'],
+                self::GET_LAST_REPORTS['request']);
+            $r->addRoute(self::ADD_REPORT['method'],
+                self::ADD_REPORT['uri'],
+                self::ADD_REPORT['request']);
+            $r->addRoute(self::GET_REPORT_BY_ID['method'],
+                self::GET_REPORT_BY_ID['uri'],
+                self::GET_REPORT_BY_ID['request']);
+            $r->addRoute(self::DELETE_REPORT['method'],
+                self::DELETE_REPORT['uri'],
+                self::DELETE_REPORT['request']);
+            $r->addRoute(self::GET_LAST_REPORT_BY_LOCATION['method'],
+                self::GET_LAST_REPORT_BY_LOCATION['uri'],
+                self::GET_LAST_REPORT_BY_LOCATION['request']);
+            $r->addRoute(self::GET_REPORTS_BY_LOCATION_BY_TIME_RANGE['method'],
+                self::GET_REPORTS_BY_LOCATION_BY_TIME_RANGE['uri'],
+                self::GET_REPORTS_BY_LOCATION_BY_TIME_RANGE['request']);
+            $r->addRoute(self::ADD_USER['method'],
+                self::ADD_USER['uri'],
+                self::ADD_USER['request']);
+            $r->addRoute(self::GET_USER_BY_EMAIL['method'],
+                self::GET_USER_BY_EMAIL['uri'],
+                self::GET_USER_BY_EMAIL['request']);
+            $r->addRoute(self::UPDATE_USER['method'],
+                self::UPDATE_USER['uri'],
+                self::UPDATE_USER['request']);
+            $r->addRoute(self::DELETE_USER['method'],
+                self::DELETE_USER['uri'],
+                self::DELETE_USER['request']);
+            $r->addRoute(self::AUTHENTICATE_USER['method'],
+                self::AUTHENTICATE_USER['uri'],
+                self::AUTHENTICATE_USER['request']);
         });
         return $dispatcher->dispatch($requestMethod, $route);
     }
