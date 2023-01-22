88be2d823e48465733d0ec35394a49a3f402a12d
Léo Paillard <leo.paillard@gmail.com>
2023-01-22 23:17:24 +0100

    implementing jwt support

--- src/utility/HttpHelper.php
+++ src/utility/HttpHelper.php
@@ -3,12 +3,23 @@
 namespace Utility;
 abstract class HttpHelper
 {
-    public static function setHttpResponse(int $httpCode, mixed $state, bool $sendRequestState): void
+    public static function setResponse(int $httpCode, mixed $state, bool $sendRequestResultState): void
     {
         http_response_code($httpCode);
         header('Content-Type: application/json');
-        if ($sendRequestState) {
+        if ($sendRequestResultState) {
             echo json_encode(['result' => $state]);
+        }
+    }
+
+    public static function getAuthHeader(): array|false
+    {
+        $headers = $_SERVER;
+        if (isset($headers['Authorization'])) {
+            $bearerToken = $headers['Authorization'];
+            return explode(' ', $bearerToken)[1];
+        } else {
+            return false;
         }
     }
 
