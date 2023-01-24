f57e19d7446968627422441cba28b63481a0c886
Léo Paillard <leo.paillard@gmail.com>
2023-01-24 22:03:04 +0100

    insert dataset for testing

--- scripts/initialize_db_insert_data_set.sql
+++ scripts/initialize_db_insert_data_set.sql
@@ -42,3 +42,41 @@ CREATE INDEX report_deviceUuid_idx ON REPORT(deviceUuid);
 CREATE INDEX report_location_idx ON REPORT(locationName);
 CREATE UNIQUE INDEX device_uuid_idx ON DEVICE(deviceUuid);
 CREATE UNIQUE INDEX user_userEmail_idx ON USER(userEmail);
+
+INSERT INTO LOCATION (locationName, latitude, longitude) VALUES
+('lyon', 45.7604, 4.84966);
+
+INSERT INTO DEVICE (deviceUuid, token, model, locationName) VALUES
+('78bccd19e274r', 'c56hs8uu675fr9gcxx6684332', 'ESP8266-O1S', 'lyon');
+
+INSERT INTO REPORT (temperature, humidity, dateTime, deviceUuid, locationName) VALUES
+(22.34, 20.50, NOW(), '78bccd19e274r', 'lyon'),
+(23.12, 21.60, NOW(), '78bccd19e274r', 'lyon'),
+(24.56, 22.10, NOW(), '78bccd19e274r', 'lyon'),
+(25.78, 20.50, NOW(), '78bccd19e274r', 'lyon'),
+(26.12, 21.60, NOW(), '78bccd19e274r', 'lyon'),
+(22.34, 20.50, NOW(), '78bccd19e274r', 'lyon'),
+(23.12, 21.60, NOW(), '78bccd19e274r', 'lyon'),
+(24.56, 22.10, NOW(), '78bccd19e274r', 'lyon'),
+(25.78, 20.50, NOW(), '78bccd19e274r', 'lyon'),
+(26.12, 21.60, NOW(), '78bccd19e274r', 'lyon'),
+(22.34, 20.50, NOW(), '78bccd19e274r', 'lyon'),
+(23.12, 21.60, NOW(), '78bccd19e274r', 'lyon'),
+(24.56, 22.10, NOW(), '78bccd19e274r', 'lyon'),
+(25.78, 20.50, NOW(), '78bccd19e274r', 'lyon'),
+(26.12, 21.60, NOW(), '78bccd19e274r', 'lyon'),
+(22.34, 20.50, NOW(), '78bccd19e274r', 'lyon'),
+(23.12, 21.60, NOW(), '78bccd19e274r', 'lyon'),
+(24.56, 22.10, NOW(), '78bccd19e274r', 'lyon'),
+(25.78, 20.50, NOW(), '78bccd19e274r', 'lyon'),
+(26.12, 21.60, NOW(), '78bccd19e274r', 'lyon'),
+(22.34, 20.50, NOW(), '78bccd19e274r', 'lyon'),
+(23.12, 21.60, NOW(), '78bccd19e274r', 'lyon'),
+(24.56, 22.10, NOW(), '78bccd19e274r', 'lyon'),
+(25.78, 20.50, NOW(), '78bccd19e274r', 'lyon'),
+(26.12, 21.60, NOW(), '78bccd19e274r', 'lyon'),
+(22.34, 20.50, NOW(), '78bccd19e274r', 'lyon'),
+(23.12, 21.60, NOW(), '78bccd19e274r', 'lyon'),
+(24.56, 22.10, NOW(), '78bccd19e274r', 'lyon'),
+(25.78, 20.50, NOW(), '78bccd19e274r', 'lyon'),
+(26.12, 21.60, NOW(), '78bccd19e274r', 'lyon');
