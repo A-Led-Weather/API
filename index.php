d799244c93cbf748d3ad34ce7816036da8a38f61
Léo Paillard <leo.paillard@gmail.com>
2023-02-10 09:57:12 +0100

    Allow CORS

--- index.php
+++ index.php
@@ -11,6 +11,11 @@ use Controller\UserController;
 use Router\Router;
 use Utility\DbConnector;
 
+// allow CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
